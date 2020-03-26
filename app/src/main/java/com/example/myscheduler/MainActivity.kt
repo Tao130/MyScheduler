934a9208e7cd72b0e366a54bbbf74b57026c7d0e
Tao130 <momossy5@gmail.com>
2020-03-26 15:45:54 +0900

    RecyclerViewのアダプターを作るところまで

---
+++ app/src/main/java/com/example/myscheduler/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myscheduler
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import io.realm.Realm
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+}
