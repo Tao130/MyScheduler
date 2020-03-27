b67184fe856a3cd7de0aba72eb2d245d534999b2
Tao130 <momossy5@gmail.com>
2020-03-27 15:49:58 +0900

    データを保存する処理の追加

--- app/src/main/java/com/example/myscheduler/MainActivity.kt
+++ app/src/main/java/com/example/myscheduler/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.myscheduler
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.LinearLayout
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.realm.Realm
+import io.realm.kotlin.where
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var realm: Realm
@@ -17,10 +23,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         realm = Realm.getDefaultInstance()
+        list.layoutManager = LinearLayoutManager(this)
+        val schedules = realm.where<Schedule>().findAll()
+        val adapter = ScheduleAdapter(schedules)
+        list.adapter = adapter
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val intent = Intent(this, ScheduleEditActivity::class.java)
+            startActivity(intent)
         }
     }
 
